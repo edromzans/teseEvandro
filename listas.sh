46b36420eca6e90b1209813ce51d27ab604f6b37
Evandro M Anselmo <evandromoimaz@gmail.com>
2014-09-11 12:10:18 +0000

    Descrevendo a severidade, agora com a base de dados de nuvens maior

--- listas.sh
+++ listas.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 makeindex -s teseEvandro.ist -t teseEvandro.sgl -o teseEvandro.sbl teseEvandro.sim
 makeindex -s teseEvandro.ist -t teseEvandro.agl -o teseEvandro.sgl teseEvandro.sig
+
+#find ./ -name '*.ps' -exec pstoimg -type png -crop a -density 300 -antialias -aaliastext {} \;
