dc4a47f3c664b4f72bf0e6bc7f953bef88fc91bd
Evandro M Anselmo <evandromoimaz@gmail.com>
2014-12-15 21:24:39 +0000

    Corrigindo a Severidade. Correcao de imagens e insercao

--- listas.sh
+++ listas.sh
@@ -2,4 +2,5 @@
 makeindex -s teseEvandro.ist -t teseEvandro.sgl -o teseEvandro.sbl teseEvandro.sim
 makeindex -s teseEvandro.ist -t teseEvandro.agl -o teseEvandro.sgl teseEvandro.sig
 
-#find ./ -name '*.ps' -exec pstoimg -type png -crop a -density 300 -antialias -aaliastext {} \;
+
+#find ./ -name '*.ps' -exec pstoimg -type png -crop a -density 600 -aaliastext -color 24 -debug  {} \;
